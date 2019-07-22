22d390eacce24d0040cfed2b098d86f07a4c9420
Mollylove <mollylovepzk@163.com>
2019-07-23 04:12:54 +0900

    Home page web

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
@@ -32,4 +34,22 @@ public class MainController {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
+
+	@RequestMapping(value = "/thy_test", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model, @RequestParam(value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber)
+	{
+		int page =1 ;
+
+		User user = userRepository.findByName("mo1");
+
+		//分页查询数据
+		model.addAttribute("page", page);
+		//查询条件
+		model.addAttribute("user", user);
+		//页面标题
+		model.addAttribute("title", "用户管理");
+		//转到待渲染模板，所有模板都在templates文件夹下，users/list指templates文件夹下的users文件夹下的list.html页面。
+		return "list";
+	}
 }
+
--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     User findByName(String name);
 
     User findByNameAndPassword(String name,String password);
+
+
 }
